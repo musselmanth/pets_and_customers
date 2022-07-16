b65c15bf19b7e281ec17ab5df2b3a63a8deab17c
Thomas Musselman <tmusselman@gmail.com>
2022-07-16 16:06:43 -0600

    feat: implement pet class and passed all tests

---
+++ lib/pet.rb
@@ -0,0 +1,22 @@
+class Pet
+
+  attr_reader :name,
+              :type,
+              :age
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @type = attributes[:type]
+    @age = attributes[:age]
+    @fed = false
+  end
+
+  def fed?
+    @fed
+  end
+
+  def feed
+    @fed = true
+  end
+
+end
